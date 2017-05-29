044f6075d6542c79db05ee739fed100ceb9bd4eb
yonkokilasi <yonkokilasi@gmail.com>
2017-05-29 12:52:26 +0300

    Integrated API now fully functional responses are in the logcat. Created Restaurant class with various constructors

--- app/src/main/java/com/example/bubbles/myrestaurants/YelpService.java
+++ app/src/main/java/com/example/bubbles/myrestaurants/YelpService.java
@@ -1,13 +1,31 @@
 package com.example.bubbles.myrestaurants;
 
-import javax.security.auth.callback.Callback;
+import okhttp3.Callback;
+
+import okhttp3.Call;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 /**
  * Created by Josephine Menge on 29/05/2017.
  */
 
 public class YelpService {
+    public static OkHttpClient client = new OkHttpClient();
     public static void findRestaurants(String location, Callback callback) {
 
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER,location);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .header("Authorization","Bearer " + Constants.YELP_ACCESS_TOKEN)
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
