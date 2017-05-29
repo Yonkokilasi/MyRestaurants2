98ba0308bc7fccf2dae292ea407be4094c0eaf0e
yonkokilasi <yonkokilasi@gmail.com>
2017-05-29 11:10:50 +0300

    Created yelp service file

---
+++ app/src/main/java/com/example/bubbles/myrestaurants/YelpService.java
@@ -0,0 +1,13 @@
+package com.example.bubbles.myrestaurants;
+
+import javax.security.auth.callback.Callback;
+
+/**
+ * Created by Josephine Menge on 29/05/2017.
+ */
+
+public class YelpService {
+    public static void findRestaurants(String location, Callback callback) {
+
+    }
+}
